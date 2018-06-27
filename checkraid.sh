f94a56f643ed9527b5fb96fbe5f66e077535d4a5
Matt Switlik <switlikm@gmail.com>
2018-06-27 16:30:40 -0400

    checkraid detect failure.

--- checkraid.sh
+++ checkraid.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-mdadm --detail /dev/md127 | grep "Failed Devices"
+FAILEDDEVICES=$(sudo mdadm --detail /dev/md127 | grep -oE  'Failed Devices : ([0-9]*)')
+#echo "'$FAILEDDEVICES'"
+if [ "$FAILEDDEVICES" = "Failed Devices : 0" ]; then
+	echo "No failed devices."
+else
+	echo "FAILED DEVICES!!!"
+fi
+
